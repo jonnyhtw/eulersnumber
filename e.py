123edcbb160bb39412e5366a57ed55f79d216fda
Jonny Williams <jonny.williams@niwa.co.nz>
2021-12-18 14:01:23 +1300

    wow it works

--- e.py
+++ e.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-sum=0
-number =1
-sum+=np.random.uniform(0,1)
+n=10000
+totalnumber=np.zeros(n)
 
-while sum < 1:
-    number+=1 
-    sum += np.random.uniform(0,1)
+for i in range(n):
 
+    sum=0
+    number=0
 
+    while sum < 1:
+        number+=1 
+        sum += np.random.uniform(0,1)
+    
+    if i ==0:
+        totalnumber[i] = number
+    if i > 0 :
+        totalnumber[i] = totalnumber[i-1]+number
+
+plt.semilogx(np.arange(1,n+1), totalnumber/np.arange(1,n+1))
+
+plt.savefig('e.png')
+
+plt.show();
